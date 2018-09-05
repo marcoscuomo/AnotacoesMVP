b2b4e0c7f280d7f013ca269b1e8898469a45cf39
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-05 12:31:17 -0300

    Cria a classe StateMaintainer

--- app/src/main/java/br/com/mojumob/anotacoesmvp/Nota.java
+++ app/src/main/java/br/com/mojumob/anotacoesmvp/Nota.java
@@ -1,4 +1,18 @@
 package br.com.mojumob.anotacoesmvp;
 
 class Nota {
+    private String text;
+    private String date;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
 }
