695d82e2c179260036a353efc7d8a48ed1e24e3a
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-05 11:14:21 -0300

    Implementa os metodos em MainModel

--- app/src/main/java/br/com/mojumob/anotacoesmvp/MainModel.java
+++ app/src/main/java/br/com/mojumob/anotacoesmvp/MainModel.java
@@ -1,6 +1,35 @@
 package br.com.mojumob.anotacoesmvp;
 
 class MainModel implements MainMVP.ModelOps {
+
+    //Referencia para layer Presenter
+    private MainMVP.RequiredPresenterOps mPresenter;
+
     public MainModel(MainPresenter mainPresenter) {
+    }
+
+
+    //Insere nota no BD
+    @Override
+    public void insereNota(Nota nota) {
+        //Logica para inserir no BD
+    }
+
+
+    //remove nota do BD
+    @Override
+    public void removeNota(Nota nota) {
+        //Logica para remover a nota do BD
+        //...
+        mPresenter.onNotaInserida(nota);
+    }
+
+    /*
+     * Disparado pelo MainPresenter#onDestroy para as operações
+     * necessárias que eventualmente estiverem executadas no BG
+     * */
+    @Override
+    public void onDestroy() {
+
     }
 }
