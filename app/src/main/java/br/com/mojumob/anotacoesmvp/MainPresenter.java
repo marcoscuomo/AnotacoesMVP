581bfe135d3a05e3732ee80462b708953af2ed1f
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-05 11:14:05 -0300

    Implementa os metodos em MainPresenter

--- app/src/main/java/br/com/mojumob/anotacoesmvp/MainPresenter.java
+++ app/src/main/java/br/com/mojumob/anotacoesmvp/MainPresenter.java
@@ -20,38 +20,75 @@ public class MainPresenter implements MainMVP.RequiredPresenterOps, MainMVP.Pres
         this.mModel = new MainModel(this);
     }
 
+    /*
+    * Disparado por Activity após a mundança de configurtação
+    * @param view Referência para a view
+    * */
     @Override
     public void onConfigurationChanged(MainMVP.RequiredViewOps view) {
-
+        mView = new WeakReference<>(view);
     }
 
+
+    /*
+    * Recebe o evento {@link MainActivity#onDestroy}
+    * @param isChangingConfig se está mudando de config
+    * */
     @Override
     public void onDestroy(boolean isChangingConfig) {
-
+        mView = null;
+        mIsChagingConfig = isChangingConfig;
+        if(!isChangingConfig){
+            mModel.onDestroy();
+        }
     }
 
+    /*
+    * Chamado pelo MainActivity quando o usuario pedi uma nova nota
+    * */
     @Override
     public void novaNota(String textoNota) {
-
+        Nota nota = new Nota();
+        nota.setText(textoNota);
+        nota.setDate(getDate());
+        mModel.insereNota(nota);
     }
 
+    /*
+    * Chamado pelo MainActivity para a remoção da nota
+    * */
     @Override
     public void deletaNota(Nota nota) {
-
+        mModel.removeNota(nota);
     }
 
+    /*
+    * Recebe o pedido do MainModel quando a nota for inserida com sucesso no BD
+    * */
     @Override
     public void onNotaInserida(Nota novaNota) {
-
+        mView.get().showToast("Novo Registro " + novaNota.getDate());
     }
 
+    /*
+    * Recebe o chamado do MainModel quando a nota for removida do BD
+    * */
     @Override
     public void onNotaRemovida(Nota notaRemovida) {
-
+        mView.get().showToast("Nota de " + notaRemovida.getDate() + " removida");
     }
 
+    /*
+    * Recebe eventuais erros do modelo e repassa a msg para o usuario
+    * */
     @Override
     public void onError(String errorMsg) {
+        mView.get().showAlert(errorMsg);
+    }
 
+    /*Retorna a data atual*/
+    private String getDate(){
+        String hoje = "hoje";
+        return hoje;
     }
 }
