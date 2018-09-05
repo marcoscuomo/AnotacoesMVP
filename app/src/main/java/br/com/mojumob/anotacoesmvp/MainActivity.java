1efbe979180632bd728f9467405b7bb4ca20796d
bytecore@gmail.com <marcos@mojumob.com.br>
2018-09-05 12:38:04 -0300

    Implementa a MainActivity

--- app/src/main/java/br/com/mojumob/anotacoesmvp/MainActivity.java
+++ app/src/main/java/br/com/mojumob/anotacoesmvp/MainActivity.java
@@ -2,12 +2,95 @@ package br.com.mojumob.anotacoesmvp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+            implements MainMVP.RequiredViewOps{
+
+    protected final String TAG = getClass().getSimpleName();
+
+    // Responsável por manter estado dos objetos inscritos
+    // durante mudanças de configuração
+    private final StateMaintainer mStateMaintainer =
+            new StateMaintainer( this.getFragmentManager(), TAG );
+
+    // Operações no Presenter
+    private MainMVP.PresenterOps mPresenter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startMVPOps();
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    }
+
+
+    /**
+     * Inicia e reinicia o Presenter. Este método precisa ser chamado
+     * após {@link Activity#onCreate(Bundle)}
+     */
+    public void startMVPOps() {
+        try {
+            if ( mStateMaintainer.firstTimeIn() ) {
+                Log.d(TAG, "onCreate() chamado pela primera vez");
+                initialize(this);
+            } else {
+                Log.d(TAG, "onCreate() chamado mais de uma vez");
+                reinitialize(this);
+            }
+        } catch ( InstantiationException | IllegalAccessException e ) {
+            Log.d(TAG, "onCreate() " + e );
+            throw new RuntimeException( e );
+        }
+    }
+
+
+    /**
+     * Inicializa os objetos relevantes para o MVP.
+     * Cria uma instância do Presenter, salva o presenter
+     * no {@link StateMaintainer} e informa à instância do
+     * presenter que objeto foi criado.
+     * @param view      Operações no View exigidas pelo Presenter
+     */
+    private void initialize( MainMVP.RequiredViewOps view )
+            throws InstantiationException, IllegalAccessException{
+        mPresenter = new MainPresenter(view);
+        mStateMaintainer.put(MainMVP.PresenterOps.class.getSimpleName(), mPresenter);
+    }
+
+    /**
+     * Recupera o presenter e informa à instância que houve uma mudança
+     * de configuração no View.
+     * Caso o presenter tenha sido perdido, uma nova instância é criada
+     */
+    private void reinitialize( MainMVP.RequiredViewOps view)
+            throws InstantiationException, IllegalAccessException {
+        mPresenter = mStateMaintainer.get( MainMVP.PresenterOps.class.getSimpleName() );
+
+        if ( mPresenter == null ) {
+            Log.w(TAG, "recriando o Presenter");
+            initialize( view );
+        } else {
+            mPresenter.onConfigurationChanged( view );
+        }
+    }
+
+
+    // Exibe AlertDialog
+    @Override
+    public void showAlert(String msg) {
+        // show alert Box
+    }
+
+    // Exibe Toast
+    @Override
+    public void showToast(String msg) {
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show;
     }
 }
